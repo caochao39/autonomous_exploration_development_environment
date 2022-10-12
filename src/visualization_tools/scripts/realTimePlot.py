e40837201cd82133586fdf2cf41617174c5f3877
Ninaad <ninaaddamis@gmail.com>
2022-10-12 05:34:13 -0400

    visualization tools

--- src/visualization_tools/scripts/realTimePlot.py
+++ src/visualization_tools/scripts/realTimePlot.py
@@ -4,7 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-import rospy
+import rclpy
+from rclpy.node import Node
 from std_msgs.msg import Float32
 
 mpl.rcParams['toolbar'] = 'None'
@@ -48,65 +49,73 @@ def travelingDistanceCallback(msg):
     global traveling_distance
     traveling_distance = msg.data
 
-def listener():
-  global time_duration, start_time_duration, explored_volume, traveling_distance, run_time, max_explored_volume, max_traveling_diatance, max_run_time, time_list1, time_list2, time_list3, run_time_list, explored_volume_list, traveling_distance_list
+def listener(args=None):
+    global time_duration, start_time_duration, explored_volume, traveling_distance, run_time, max_explored_volume, max_traveling_diatance, max_run_time, time_list1, time_list2, time_list3, run_time_list, explored_volume_list, traveling_distance_list
 
-  rospy.init_node('realTimePlot')
-  rospy.Subscriber("/time_duration", Float32, timeDurationCallback)
-  rospy.Subscriber("/runtime", Float32, runTimeCallback)
-  rospy.Subscriber("/explored_volume", Float32, exploredVolumeCallback)
-  rospy.Subscriber("/traveling_distance", Float32, travelingDistanceCallback)
+    #rospy.init_node('realTimePlot')
+    rclpy.init(args=args)
+    node = rclpy.create_node('realTimePlot')
+    # rospy.Subscriber("/time_duration", Float32, timeDurationCallback)
+    timeDurationSub = node.create_subscription(Float32, '/time_duration', timeDurationCallback,5)
+    # rospy.Subscriber("/runtime", Float32, runTimeCallback)
+    runTimeSub = node.create_subscription(Float32, '/runtime', runTimeCallback,5)
+    # rospy.Subscriber("/explored_volume", Float32, exploredVolumeCallback)
+    exploredVolumeSub = node.create_subscription(Float32, '/explored_volume', exploredVolumeCallback,5)
+    # rospy.Subscriber("/traveling_distance", Float32, travelingDistanceCallback)
+    travellingDistanceSub = node.create_subscription(Float32, '/traveling_distance', travelingDistanceCallback,5)
 
-  fig=plt.figure(figsize=(8,7))
-  fig1=fig.add_subplot(311)
-  plt.title("Exploration Metrics\n", fontsize=14)
-  plt.margins(x=0.001)
-  fig1.set_ylabel("Explored\nVolume (m$^3$)", fontsize=12)
-  l1, = fig1.plot(time_list2, explored_volume_list, color='r', label='Explored Volume')
-  fig2=fig.add_subplot(312)
-  fig2.set_ylabel("Traveling\nDistance (m)", fontsize=12)
-  l2, = fig2.plot(time_list3, traveling_distance_list, color='r', label='Traveling Distance')
-  fig3=fig.add_subplot(313)
-  fig3.set_ylabel("Algorithm\nRuntime (s)", fontsize=12)
-  fig3.set_xlabel("Time Duration (s)", fontsize=12) #only set once
-  l3, = fig3.plot(time_list1, run_time_list, color='r', label='Algorithm Runtime')
+    fig=plt.figure(figsize=(8,7))
+    fig1=fig.add_subplot(311)
+    plt.title("Exploration Metrics\n", fontsize=14)
+    plt.margins(x=0.001)
+    fig1.set_ylabel("Explored\nVolume (m$^3$)", fontsize=12)
+    l1, = fig1.plot(time_list2, explored_volume_list, color='r', label='Explored Volume')
+    fig2=fig.add_subplot(312)
+    fig2.set_ylabel("Traveling\nDistance (m)", fontsize=12)
+    l2, = fig2.plot(time_list3, traveling_distance_list, color='r', label='Traveling Distance')
+    fig3=fig.add_subplot(313)
+    fig3.set_ylabel("Algorithm\nRuntime (s)", fontsize=12)
+    fig3.set_xlabel("Time Duration (s)", fontsize=12) #only set once
+    l3, = fig3.plot(time_list1, run_time_list, color='r', label='Algorithm Runtime')
 
-  count = 0
-  r = rospy.Rate(100) # 100hz
-  while not rospy.is_shutdown():
-      r.sleep()
-      count = count + 1
+    count = 0
+    r = node.create_rate(100) # 100hz
+    while rclpy.ok():
+        r.sleep()
+        count = count + 1
 
-      if count % 25 == 0:
-        max_explored_volume = explored_volume
-        max_traveling_diatance = traveling_distance
-        if run_time > max_run_time:
-            max_run_time = run_time
+        if count % 25 == 0:
+            max_explored_volume = explored_volume
+            max_traveling_diatance = traveling_distance
+            if run_time > max_run_time:
+                max_run_time = run_time
 
-        time_list2 = np.append(time_list2, time_duration)
-        explored_volume_list = np.append(explored_volume_list, explored_volume)
-        time_list3 = np.append(time_list3, time_duration)
-        traveling_distance_list = np.append(traveling_distance_list, traveling_distance)
-        time_list1 = np.append(time_list1, time_duration)
-        run_time_list = np.append(run_time_list, run_time)
+            time_list2 = np.append(time_list2, time_duration)
+            explored_volume_list = np.append(explored_volume_list, explored_volume)
+            time_list3 = np.append(time_list3, time_duration)
+            traveling_distance_list = np.append(traveling_distance_list, traveling_distance)
+            time_list1 = np.append(time_list1, time_duration)
+            run_time_list = np.append(run_time_list, run_time)
 
-      if count >= 100:
-        count = 0
-        l1.set_xdata(time_list2)
-        l2.set_xdata(time_list3)
-        l3.set_xdata(time_list1)
-        l1.set_ydata(explored_volume_list)
-        l2.set_ydata(traveling_distance_list)
-        l3.set_ydata(run_time_list)
+        if count >= 100:
+            count = 0
+            l1.set_xdata(time_list2)
+            l2.set_xdata(time_list3)
+            l3.set_xdata(time_list1)
+            l1.set_ydata(explored_volume_list)
+            l2.set_ydata(traveling_distance_list)
+            l3.set_ydata(run_time_list)
 
-        fig1.set_ylim(0, max_explored_volume + 500)
-        fig1.set_xlim(start_time_duration, time_duration + 10)
-        fig2.set_ylim(0, max_traveling_diatance + 20)
-        fig2.set_xlim(start_time_duration, time_duration + 10)
-        fig3.set_ylim(0, max_run_time + 0.2)
-        fig3.set_xlim(start_time_duration, time_duration + 10)
+            fig1.set_ylim(0, max_explored_volume + 500)
+            fig1.set_xlim(start_time_duration, time_duration + 10)
+            fig2.set_ylim(0, max_traveling_diatance + 20)
+            fig2.set_xlim(start_time_duration, time_duration + 10)
+            fig3.set_ylim(0, max_run_time + 0.2)
+            fig3.set_xlim(start_time_duration, time_duration + 10)
 
-        fig.canvas.draw()
+            fig.canvas.draw()
+    node.destroy_node()
+    rclpy.shutdown() 
 
 if __name__ == '__main__':
   listener()
