0a331d385e4a50c459888bdcdc97a671bb02c2a8
Ninaad <ninaaddamis@gmail.com>
2022-10-12 05:37:02 -0400

    waypoint_rviz_plugin

--- src/waypoint_rviz_plugin/src/waypoint_tool.cpp
+++ src/waypoint_rviz_plugin/src/waypoint_tool.cpp
@@ -1,18 +1,19 @@
-#include "waypoint_tool.h"
+#include <waypoint_rviz_plugin/waypoint_tool.h>
+// #include <waypoint_tool.h>
 
-namespace rviz
+namespace waypoint_rviz_plugin
 {
 WaypointTool::WaypointTool()
 {
   shortcut_key_ = 'w';
 
-  topic_property_ = new StringProperty("Topic", "waypoint", "The topic on which to publish navigation waypionts.",
+  topic_property_ = new rviz_common::properties::StringProperty("Topic", "waypoint", "The topic on which to publish navigation waypionts.",
                                        getPropertyContainer(), SLOT(updateTopic()), this);
 }
 
 void WaypointTool::onInitialize()
 {
-  PoseTool::onInitialize();
+  rviz_default_plugins::tools::PoseTool::onInitialize();
   setName("Waypoint");
   updateTopic();
   vehicle_z = 0;
@@ -20,19 +21,23 @@ void WaypointTool::onInitialize()
 
 void WaypointTool::updateTopic()
 {
-  sub_ = nh_.subscribe<nav_msgs::Odometry> ("/state_estimation", 5, &WaypointTool::odomHandler, this);
-  pub_ = nh_.advertise<geometry_msgs::PointStamped>("/way_point", 5);
-  pub_joy_ = nh_.advertise<sensor_msgs::Joy>("/joy", 5);
+  // sub_ = nh_.subscribe<nav_msgs::Odometry> ("/state_estimation", 5, &WaypointTool::odomHandler, this);
+  // sub_ = nh_->create_subscription<nav_msgs::msg::Odometry> ("/state_estimation", 5, &rviz_default_plugins::tools::WaypointTool::odomHandler, nh_);
+  // sub_ = nh_->create_subscription<nav_msgs::msg::Odometry>("/state_estimation", 5 ,std::bind(&WaypointTool::odomHandler,nh_,std::placeholders::_1));
+  // pub_ = nh_.advertise<geometry_msgs::PointStamped>("/way_point", 5);
+  pub_ = nh_->create_publisher<geometry_msgs::msg::PointStamped>("/way_point", 5);
+  // pub_joy_ = nh_.advertise<sensor_msgs::Joy>("/joy", 5);
+  pub_joy_ = nh_->create_publisher<sensor_msgs::msg::Joy>("/joy", 5);
 }
 
-void WaypointTool::odomHandler(const nav_msgs::Odometry::ConstPtr& odom)
+void WaypointTool::odomHandler(const nav_msgs::msg::Odometry::ConstSharedPtr odom)
 {
   vehicle_z = odom->pose.pose.position.z;
 }
 
 void WaypointTool::onPoseSet(double x, double y, double theta)
 {
-  sensor_msgs::Joy joy;
+  sensor_msgs::msg::Joy joy;
 
   joy.axes.push_back(0);
   joy.axes.push_back(0);
@@ -55,22 +60,24 @@ void WaypointTool::onPoseSet(double x, double y, double theta)
   joy.buttons.push_back(0);
   joy.buttons.push_back(0);
 
-  joy.header.stamp = ros::Time::now();
+  // joy.header.stamp = ros::Time::now();
+  joy.header.stamp = nh_->now();
   joy.header.frame_id = "waypoint_tool";
-  pub_joy_.publish(joy);
+  pub_joy_->publish(joy);
 
-  geometry_msgs::PointStamped waypoint;
+  geometry_msgs::msg::PointStamped waypoint;
   waypoint.header.frame_id = "map";
   waypoint.header.stamp = joy.header.stamp;
   waypoint.point.x = x;
   waypoint.point.y = y;
   waypoint.point.z = vehicle_z;
 
-  pub_.publish(waypoint);
+  pub_->publish(waypoint);
   usleep(10000);
-  pub_.publish(waypoint);
+  pub_->publish(waypoint);
 }
 }
 
 #include <pluginlib/class_list_macros.hpp>
-PLUGINLIB_EXPORT_CLASS(rviz::WaypointTool, rviz::Tool)
+// PLUGINLIB_EXPORT_CLASS(rviz::WaypointTool, rviz::Tool)
+PLUGINLIB_EXPORT_CLASS(waypoint_rviz_plugin::WaypointTool, rviz_default_plugins::tools::PoseTool)
